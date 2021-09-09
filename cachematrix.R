421648f47f5412c7505f62f079338118695b5077
MasterMS02 <90375450+MasterMS02@users.noreply.github.com>
2021-09-09 08:48:59 +0200

    CourseraProgrammingAssignment2MaximilianSenk

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Building a matrix inversion could take a lot of time. To minimize 
+## the computation time of building a matrix inversion
+## you can use the 'makeCacheMatrix' function to store a matrix 
+## in the cache. Later on you can check in the second function
+## whether the matrix inversion was already built. Then the 
+## function takes the matrix inversion that already exists in the 
+## cache. So the function do not has to build it again.
 
-## Write a short comment describing this function
+## Creates a cache matrix that allows you to access on a matrix
+## that exists in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse_matrix <- NULL
+  set <- function(y){
+    x <<- y
+    inverse_matrix <<- NULL
+  }
+  get <- function() x
+  setInverseMatrix <- function(inverseMatrix) inverse_matrix <<- inverseMatrix
+  getInverseMatrix <- function() inverse_matrix
+  list(set = set, get = get, 
+       setInverseMatrix = setInverseMatrix, 
+       getInverseMatrix = getInverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## If there already exists a matrix in the cache, the function
+## returns the existing matrix inversion. Otherwise the function
+## builds a new matrix inversion and returns its value.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inverse_matrix <- x$getInverseMatrix()
+  if(!is.null(inverse_matrix)){
+    message("getting cached data")
+    return(inverse_matrix)
+  }
+  data <- x$get()
+  inverse_matrix <- solve(data, ...)
+  x$setInverseMatrix(inverse_matrix)
+  inverse_matrix
 }
